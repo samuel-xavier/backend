3557d7ae6749a0b75df33503cfff8a9bd00b62a6
Samuel-Xavier <samueloliveira.xavier@hotmail.com>
2022-02-19 08:51:59 -0300

    2022 02 19 0851

--- src/controllers/Tp_analisesController.js
+++ src/controllers/Tp_analisesController.js
@@ -29,6 +29,25 @@ module.exports = {
         return res.json(tp_analises);
     },
 
+    async index_perfil(req, res) { 
+
+        const { cdempresa, perfil } = req.params;
+
+        var qry = '';
+        qry = qry + 'SELECT tp_analises.ID AS id, tp_analises.CDEMPRESA AS cdempresa, ';
+        qry = qry +   'tp_analises.TP_ANALISE AS tp_analise, tp_analises.DS_ANALISE AS ds_analise, ';
+        qry = qry +   'tp_analises.DT_ANALISE AS dt_analise, tp_analises.HR_ANALISE AS hr_analise ';
+        qry = qry + 'FROM tp_analises ';
+        qry = qry + 'WHERE tp_analises.CDEMPRESA = ' + cdempresa + '" ';
+        qry = qry +   'AND tp_analises.TP_ANALISE = ' + perfil;
+
+        const tp_analises = await connection.query( qry,
+            { type: Sequelize.QueryTypes.SELECT }
+            );
+
+        return res.json(tp_analises);
+    },
+
 
     async store(req, res) {
         const { cdempresa, tp_analise, ds_analise, dt_analise, hr_analise } = req.body;
